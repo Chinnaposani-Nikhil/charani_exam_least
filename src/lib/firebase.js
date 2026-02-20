b33df91156b6895e19eab4a922081fb65a74da8d
nikhil-charani <nikhilch.charani@gmail.com>
2026-02-20 17:29:51 +0530

    added new mohan fire database

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -36,7 +36,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  databaseURL:'https://charani-online-test-db-default-rtdb.firebaseio.com',
+  databaseURL:'https://charani-online-exam-default-rtdb.firebaseio.com',
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
